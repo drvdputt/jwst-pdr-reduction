3fbcc9f46676b7a1a289114f4a40ebd83bb37e4c
Dries Van De Putte <dvandepu@uwo.ca>
2024-09-05 11:56:13 -0400

    Add output_type band to cube_build_ch1wcs_orion script

--- shell_scripts/cube_build_ch1wcs_orion.sh
+++ shell_scripts/cube_build_ch1wcs_orion.sh
@@ -3,4 +3,4 @@
 # one association file per nirspec filter. I feel it's a bit more memory efficient that way. Can
 # build the cubes one by bone.
 mkdir -p ch1wcs
-strun cube_build $1 --output_dir ch1wcs --cube_pa=250.42338204969806 --scalexy 0.13 --ra_center 83.83535169747073 --dec_center -5.419729392828107  --nspax_x 213 --nspax_y 41
+strun cube_build $1 --output_dir ch1wcs --cube_pa=250.42338204969806 --scalexy 0.13 --ra_center 83.83535169747073 --dec_center -5.419729392828107  --nspax_x 213 --nspax_y 41 --output_type='band'
